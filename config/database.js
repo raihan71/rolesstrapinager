d9ea9966c598e0fbca7a021623cf3aceb76fac06
raihan <raihannismara@gmail.com>
2020-07-02 19:01:50 +0700

    Changes database

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'rolesstrapinager'),
-        username: env('DATABASE_USERNAME', 'root'),
+        username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
