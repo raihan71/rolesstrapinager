9c9eecabaa5d3dc3f2fee08963e38759d15b4688
raihan <raihannismara@gmail.com>
2020-07-02 19:58:15 +0700

    changed to postgresql

--- config/database.js
+++ config/database.js
@@ -4,14 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
+        client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'rolesstrapinager'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
       },
-      options: {}
+      options: {
+        ssl: false,
+      }
     },
   },
 });
